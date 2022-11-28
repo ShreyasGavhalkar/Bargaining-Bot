68526452c16e541acd5e950b685ff807f5fa8d51
Alok Joshi <AlokJoshi523@gmail.com>
2022-11-28 23:22:58 +0530

    Fixed data processing. Removed duplicate entries at different stages of processing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,40 +1,67 @@
 import pandas as pd
+import time
 
+
+##### UTILITY FUNCTIONS
 def discount(mrp,unit_price):
     return ((mrp-unit_price)/(mrp))*100
 
 def mrp(max_unit_price):
     return max_unit_price*1 #can be changed later
 
+def contains_alphabet(stock_code):
+    
+    for char in stock_code:
+        if(char.isalpha()):
+            return True;
+    return False;
+
+################################
+
+##### Removing nan values and duplicates
 df = pd.read_csv("data.csv",encoding = "ISO-8859-1")
 df = df.dropna() #drop all Rows with any NaN value
+df = df.drop_duplicates() #drops all duplicate rows
+################################
 
+###### Removing invalid values
 df = df[df.Quantity>0] #Remove Items with a negative Quantity value
 df = df[df.UnitPrice>0] #Remove items with Unit price <= zero (Free items)
+################################
 
+###### Dropping unwanted columns
 df = df.drop('InvoiceNo',axis = 1) #remove un
 df = df.drop('InvoiceDate',axis = 1)
 df = df.drop('Country',axis = 1)
 df = df.drop('Description',axis = 1)
+################################
 
-### Data preprocessing part over.
-### New Column addition: Discount
 
-df["discount"] = 1 #creates a new row named discount
+###### Removing StockCodes with alphabets in them
 stock_codes = list(df.StockCode.unique())
 code_maxp = dict()
 
 for stock in stock_codes:
-    code_maxp[stock] = mrp(max(df[df.StockCode == stock].UnitPrice.unique()))
+        code_maxp[stock] = mrp(max(df[df.StockCode == stock].UnitPrice.unique()))
 
+################################
+print(df)
+###### drop duplicates, create new column named discount
+df = df.drop_duplicates(subset = ['StockCode','Quantity','CustomerID'])
+print(df)
+df["discount"] = 1 #creates a new row named discount
+################################
+
+row_count = 1;
 for i in df.index:
 
     dis_val= discount(float(code_maxp[df["StockCode"][i]]),float(df["UnitPrice"][i]))
-    df.at[i,"discount"] = dis_val
-    print(df["discount"][i])
+    df.at[i,"discount"] = int(dis_val)
+    print(f"{row_count} / {len(df.index)}")
+    row_count+=1
 
 
 
 print(df)
-
+df = df.drop("UnitPrice",axis = 1)
 df.to_csv("processed.csv",header = True,index = False)
